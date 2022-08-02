fca77d195e116c6c8c669bc70430acc4aba01ae0
franco <sanabriagabriel400@gmail.com>
2022-08-02 15:00:02 -0400

    version1

---
+++ README.md
@@ -0,0 +1,15 @@
+# MASKPHISH
+
+Presentamos la tecnologia de enmascaramiento de URL al mundo por primera vez. Dar una máscara a la URL de phishing como un pro..una herramienta imprescindible para phishing.
+
+# INSTALACION
+
+$ apt update && apt upgrade -y
+
+$ pkg install git -y
+
+$ git clone https://github.com/KingHackers001
+
+$ cd Maskphish
+
+$ bash maskphish.sh
---
+++ maskphish.sh
@@ -0,0 +1,77 @@
+# Bash Script para ocultar URL de phishing creado por KP
+
+url_checker() {
+    if [ ! "${1//:*}" = http ]; then
+        if [ ! "${1//:*}" = https ]; then
+            echo -e "\e[1;31m
+┌═══════════════════════════════════════════════┐
+█  \e[31m¡¡¡\e[1;37m DATOS ERRONEOS VUELVA A EMPEZAR \e[31m¡¡¡\e[1;31m      █
+█  \e[1;37mSI DESEA SALIR PRESIONE LAS TECLAS\e[1;32m Ctrl \e[1;37m+ \e[1;32mc \e[1;31m █
+└═══════════════════════════════════════════════┘\e[0m"
+            bash maskphish.sh 1
+            exit 1
+        fi
+    fi
+}
+
+clear
+sleep 0.5
+echo -e "\n\e[1;30m┌─────────────────────────────┐ \e[0m"
+echo -e "\e[1;30m│                             \e[1;30m│\e[0m"
+echo -e "\e[1;30m│\e[1;32m  ▙▗▌      ▌  \e[1;30m▛▀▖▌  ▗    ▌   │ \e[0m"
+echo -e "\e[1;30m│\e[1;32m  ▌▘▌▝▀▖▞▀▘▌▗▘\e[1;30m▙▄▘▛▀▖▄ ▞▀▘▛▀▖ │ \e[0m"
+echo -e "\e[1;30m│\e[1;32m  ▌ ▌▞▀▌▝▀▖▛▚ \e[1;30m▌  ▌ ▌▐ ▝▀▖▌ ▌ │ \e[0m"
+echo -e "\e[1;30m│\e[1;32m  ▘ ▘▝▀▘▀▀ ▘ ▘\e[1;30m▘  ▘ ▘▀▘▀▀ ▘ ▘ │ \e[0m"
+echo -e "\e[1;30m│           KING 001 \e[1;30m│\e[0m"
+echo -e "\e[1;30m└─────────────────────────────┘ \e[0m \n"
+echo -e -n "
+\e[1;30m┌═══════════════════════════════┐
+█\e[1;32m PEGUE LA URL DE PHYSHING AQUI\e[1;30m █
+█\e[1;30m (\e[1;37mcom http o https\e[1;30m) \e[1;32m O BIEN\e[1;30m    █
+█ \e[1;31mPARA SALIR PRESIONE\e[1;32m Ctrl \e[1;31m+ \e[1;32mc\e[1;30m  █
+└═══════════════════════════════┘
+┃
+└═>>>\e[1;37m "
+read phish
+url_checker $phish
+sleep 1
+echo -e "
+\e[1;30m┌══════════════════════════════┐
+█\e[1;32m PROCESAMIENTO Y MODIFICACIÓN\e[1;30m █
+█\e[1;32m DE URL DE PHYSHING\e[1;30m           █
+└══════════════════════════════┘"
+short=$(curl -s https://is.gd/create.php\?format\=simple\&url\=${phish})
+shorter=${short#https://}
+echo -e -n "
+\e[1;30m┌══════════════════════════════════════════════┐
+█\e[1;32m DOMINIO PARA ENMASCARAR LA URL DE PHYSHING\e[1;30m   █
+█\e[1;30m (\e[1;37mcom http or https\e[1;30m)\e[1;37m, ex: https://google.com,\e[1;30m █
+█\e[1;37m http://anyting.org\e[1;30m)                          █
+└══════════════════════════════════════════════┘
+┃
+└═>>>\e[1;37m "
+read mask
+url_checker $mask
+echo -e '\n
+\e[1;30m┌════════════════════════════════════════┐
+█\e[1;32m ESCRIBA PALABRAS DE INGENIERIA SOCIAL:\e[1;30m █
+█\e[1;30m (\e[1;37mCOMO EL DINERO GRATIS, LOS MEJORES\e[1;30m    █
+█\e[1;37m TRUCOS DE PUBLICACIÓN\e[1;30m)                 █
+└════════════════════════════════════════┘'
+echo -e -n "
+\e[1;30m┌═══════════════════════════════════════┐
+█\e[1;32m NO USE EL ESPACIO, SOLO USE \e[1;30m'\e[1;37m-\e[1;30m'\e[1;32m ENTRE\e[1;30m █
+█\e[1;32m LAS PALABRAS DE INGENIERIA SOCIAL\e[1;30m     █
+└═══════════════════════════════════════┘
+┃
+└═>>>\e[1;37m "
+read words
+echo -e "\n\e[1;30m┌═══════════════════════════════════┐
+█\e[1;32m GENERANDO EL ENLACE MASKPHISH ...\e[1;30m █
+└═══════════════════════════════════┘\n"
+final=$mask-$words@$shorter
+echo -e "\e[1;30m┌═══════════════════════════════┐
+█\e[1;32m AQUI ESTA LA URL DE MASKPHISH\e[1;30m █
+└═══════════════════════════════┘
+┃
+└═>>>\e[37m ${final} \e[0m\n "
